30b92244b2e4d7fcc161c552b53866bbc0b6327c
Eriberto Guzman <eriberto176@gmail.com>
2020-04-04 09:46:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -4,3 +4,8 @@ function updatePlaylist(obj, artist, song){
   return Object.assign({}, obj, {artist: song})
   
 }
+
+function removeFromPlaylist(obj, artist){
+  delete obj.artist;
+  return obj
+}

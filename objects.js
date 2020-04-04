06f658ab867e561b5532e27fe4791fd941c76e56
Eriberto Guzman <eriberto176@gmail.com>
2020-04-04 09:50:55 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {'Kanye West': 'Jesus Walks'};
 
 function updatePlaylist(obj, artist, song){
-  var newPlaylist = Object.assign({}, obj, {artist: song})
+  Object.assign(obj, {artist: song})
   return newPlaylist
   
 }
